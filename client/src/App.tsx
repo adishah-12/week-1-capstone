376e79ae6a95a7fce1ed4ff660779f45b557fca1
adishah-12 <contact@adi.moe>
2026-09-03 12:34:16 -0400

    structure and housekeeping

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,25 @@
+// src/main.tsx or src/App.tsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Landing from './pages/Landing'
+import Browse from './pages/Browse'
+import RecipeDetail from './pages/RecipeDetail'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+
 function App() {
-  return <h1>Deloitte React Project</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/recipes" element={<Browse />} />
+        <Route path="/recipes/:id" element={<RecipeDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
